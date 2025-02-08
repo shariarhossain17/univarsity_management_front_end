9d27230867ca5f3ec4fb5fb56e6146211a96031d
shariarhossain17 <shariarhossain23@gmail.com>
2025-02-08 16:57:37 +0600

    added form provider

---
+++ src/components/Forms/Form.tsx
@@ -0,0 +1,33 @@
+import { ReactElement, ReactNode } from "react";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
+
+type FormConfig = {
+  defaultValues?: Record<string, any>;
+};
+type FormProps = {
+  children?: ReactElement | ReactNode;
+  submitHandler: SubmitHandler<any>;
+} & FormConfig;
+const Form = ({ children, submitHandler, defaultValues }: FormProps) => {
+  const formConfig: FormConfig = {};
+
+  if (!!defaultValues) formConfig["defaultValues"] = defaultValues;
+
+  const methods = useForm<FormProps>(formConfig);
+  const { handleSubmit, reset } = methods;
+
+  const onSubmit = (data: any) => {
+    submitHandler(data);
+    reset();
+  };
+
+  return (
+    <div>
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit(onSubmit)}>{children}</form>
+      </FormProvider>
+    </div>
+  );
+};
+
+export default Form;
